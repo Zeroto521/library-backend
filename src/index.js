4b9bb74fcfbf1854201b09444631dfcfd1a57a41
Zero <zeroto521@gmail.com>
2021-01-09 18:25:04 +0800

    :tada: finish 8.6

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+import { v1 as uuidv1 } from 'uuid'
 import pkg from 'apollo-server'
 
-const { ApolloServer, gql } = pkg
-
 import data from './data.js'
+
+const { ApolloServer, gql } = pkg
 
 const typeDefs = gql`
   type Author {
     id: ID!
     name: String!
-    born: Int!
-    authorCount: Int
+    born: Int
+    bookCount: Int
   }
 
   type Book {
@@ -26,6 +27,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+  addBook(
+    title: String!
+    author: String
+    published: Int!
+    genres: [String!]!
+  ): Book
+}  
 `
 
 const resolvers = {
@@ -47,8 +57,23 @@ const resolvers = {
     allAuthors: () => data.authors
   },
   Author: {
-    authorCount: (root) => {
+    bookCount: (root) => {
       return data.books.filter(book => book.author === root.name).length
+    }
+  },
+  Mutation: {
+    addBook: (_, args) => {
+      if (!data.authors.find(author => author.name === args.author)) {
+        data.authors.concat({
+          name: args.author,
+          id: uuidv1(),
+        })
+
+      }
+      const book = { ...args, id: uuidv1() }
+      data.books = data.books.concat(book)
+
+      return book
     }
   }
 }

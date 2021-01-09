d651338e19eb1333581df454ecf12e03d519b70d
Zero <zeroto521@gmail.com>
2021-01-09 18:27:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,12 +29,12 @@ const typeDefs = gql`
   }
 
   type Mutation {
-  addBook(
-    title: String!
-    author: String
-    published: Int!
-    genres: [String!]!
-  ): Book
+    addBook(
+      title: String!
+      author: String
+      published: Int!
+      genres: [String!]!
+    ): Book
 }  
 `
 
